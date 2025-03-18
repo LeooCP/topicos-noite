e0deb82e0f8f6419e69b8edefcf480e1b6738ff4
LEONARDO CALEGARI PECH <leonardo.calegari@cs.up.edu.br>
2025-03-17 21:50:46 -0300

    Teste Program Cursos

--- Positivo/API/Program.cs
+++ Positivo/API/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<Curso> cursos = new List<Curso>(){
+    new Curso(1, "Ciência da Computação"),
+    new Curso(2, "Engenharia de Software")
+};
 
-app.Run();
+app.MapGet("/", () => {
+    return Results.Ok(cursos);
+});
+
+app.MapGet("/{id}", ([FromRoute] int id) => {
+return Results.Ok(cursos.Find(curso => curso.Id == id));
+});
+
+app.MapPost("/", ([FromBody] Curso curso) => {
+    cursos.Add(curso);
+    return Results.Created(curso);
+});
+
+app.run();
